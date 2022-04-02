da9467bcbd343b6a116a422806c61178e14ccf80
jmkim <wjdals0304@gmail.com>
2022-04-02 14:36:27 +0900

    bug:  테스트 케이스 버그 수정

--- Swift-algorithm/스택,큐/valid-parentheses.playground/Contents.swift
+++ Swift-algorithm/스택,큐/valid-parentheses.playground/Contents.swift
@@ -5,6 +5,10 @@ import UIKit
 func isValid(_ s: String) -> Bool {
     var stack:[Character] = []
     
+    if s.count == 1 {
+        return false
+    }
+    
     var table: [Character:Character] = [
         ")" : "(" ,
         "}" : "{" ,
@@ -12,20 +16,19 @@ func isValid(_ s: String) -> Bool {
     ]
     
     for char in s {
+
         if !table.keys.contains(char) {
+
             stack.append(char)
-        } else if !stack.isEmpty {
-            print(stack)
-            if table[char] != stack.removeLast() {
+        } else if stack.isEmpty || table[char] != stack.popLast() {
             return false
-            }
         }
     }
-    print(stack)
     
     return stack.count == 0
 }
 
 
-print(isValid("[({}))"))
+
+print(isValid("{()}"))
 
